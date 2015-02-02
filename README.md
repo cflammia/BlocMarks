009957e12d93bab0d86c79789dd043d23bbed00b
cflammia <flammia.carrie@gmail.com>
2015-02-02 21:32:25 +0000

    Repo commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## BlocMarks: an app that that allow a user to bookmark URLs via email, peruse other user's bookmarks and maintain a personal index of categorized bookmarks.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocmarks::Application.config.secret_key_base = 'a232bc54ff166335a53d7f643842315e300bde2242481d5f9b3e1764041a78310a79bb40c6e613e4a7b39a893b9c0b9ef13ad7788b0b9e47b5da004e784e3fc3'
